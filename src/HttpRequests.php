27012247c5b369f2e13896123a1b3a308c620ccd
Steve Meyers <steve-github@spamwiz.com>
2015-01-27 12:48:27 -0700

    Initial commit of simple HTTP class

---
+++ src/HttpRequests.php
@@ -0,0 +1,181 @@
+<?
+
+namespace Cohort;
+
+class HttpRequests {
+	private $requests = [];
+	
+	public function __construct() {}
+	
+	public static function SingleRequest($url, $data = false, $type = "GET", $extra_options = false) {
+		$response = false;
+		if ($type === "GET" && $data !== false) {
+			$query = http_build_query($data);
+			if (strpos($url, "?")) {
+				$url = "$url&$query";
+			} else {
+				$url = "$url?$query";
+			}
+			$data = false;
+		}
+		
+		$options = [
+			'url' => $url,
+			"post_data" => $data,
+			"callback" => function($result) use (&$response) {
+				$response = $result;
+			},
+		];
+		if (is_array($extra_options)) {
+			foreach ($extra_options as $key => $val) {
+				$options[$key] = $val;
+			}
+		}
+		
+		$http = new HttpRequests();
+		$http->addRequest($options);
+		$http->execute();
+		
+		return $response;
+	}
+	public static function SingleGet($url, $extra_options = false) {
+		return static::SingleRequest($url, false, "GET", $extra_options);
+	}
+	public static function SinglePost($url, $data, $extra_options = false) {
+		return static::SingleRequest($url, $data, "POST", $extra_options);
+	}
+	
+	public function numRequests() {
+		return count($this->requests);
+	}
+	
+	public function addRequest($arr, $errors_ok = false) {
+		$num = $this->numRequests();
+		$req = [
+			"url" => $arr['url'],
+			"result" => false,
+			"errors_ok" => $errors_ok,
+		];
+		
+		foreach ([ "callback", "callback_info", "post_data", "cookies", "user_agent", "headers", "referer" ] as $key) {
+			$req[$key] = (isset($arr[$key]) ? $arr[$key] : false);
+		}
+		
+		$this->requests[$num] = $req;
+		return $num;
+	}
+	
+	public function execute() {
+		$mh = curl_multi_init();
+		$ch = [];
+		
+		$base_curl_opts = [
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_MAXREDIRS => 2,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_CONNECTTIMEOUT => 3,
+			CURLOPT_TIMEOUT => 10,
+			CURLOPT_SSL_VERIFYPEER => false,
+			CURLOPT_SAFE_UPLOAD => true,
+			CURLOPT_HEADER => true,
+		];
+		
+		// Create requests for each URL, including all additional information
+		foreach ($this->requests as $num => $req) {
+			$ch[$num] = curl_init($req['url']);
+			
+			$curl_opts = $base_curl_opts;
+			if (!empty($req['post_data'])) {
+				$curl_opts[CURLOPT_POST] = true;
+				$curl_opts[CURLOPT_POSTFIELDS] = $req['post_data'];
+			}
+			if (!empty($req['cookies'])) {
+				if (is_array($req['cookies'])) {
+					$tmp = [];
+					foreach ($req['cookies'] as $k => $v) {
+						$tmp[] = sprintf("%s=%s", $k, $v);
+					}
+					$req['cookies'] = implode("; ", $tmp);
+				}
+				$curl_opts[CURLOPT_COOKIE] = $req['cookies'];
+			}
+			if (!empty($req['user_agent'])) $curl_opts[CURLOPT_USERAGENT] = $req['user_agent'];
+			if (!empty($req['headers'])) $curl_opts[CURLOPT_HTTPHEADER] = $req['headers'];
+			if (!empty($req['referer'])) $curl_opts[CURLOPT_REFERER] = $req['referer'];
+			
+			curl_setopt_array($ch[$num], $curl_opts);
+			
+			curl_multi_add_handle($mh, $ch[$num]);
+		}
+		
+		// Start performing the request
+		do {
+				$exec_return_value = @curl_multi_exec($mh, $running_handles);
+		} while ($exec_return_value == CURLM_CALL_MULTI_PERFORM);
+		
+		// Loop and continue processing the request
+		while ($running_handles && $exec_return_value == CURLM_OK) {
+			// Wait forever for network
+			$num_ready = @curl_multi_select($mh);
+			if ($num_ready == -1) {
+				usleep(100);
+			}
+			
+			// Pull in any new data, or at least handle timeouts
+			do {
+				$exec_return_value = @curl_multi_exec($mh, $running_handles);
+			} while ($exec_return_value == CURLM_CALL_MULTI_PERFORM);
+			
+			if ($num_ready != -1) {
+				if ($exec_return_value !== CURLM_OK) {
+					switch ($exec_return_value) {
+						case CURLM_BAD_HANDLE: $error = "BAD HANDLE"; break;
+						case CURLM_BAD_EASY_HANDLE: $error = "BAD EASY HANDLE"; break;
+						case CURLM_OUT_OF_MEMORY: $error = "OUT OF MEMORY"; break;
+						case CURLM_INTERNAL_ERROR: $error = "INTERNAL ERROR"; break;
+						default: $error = $exec_return_value; break;
+					}
+					throw new \Exception("cURL multi-exec error: $error");
+				}
+			}
+		}
+		
+		// Check for any errors
+		if ($exec_return_value != CURLM_OK) {
+			trigger_error("Curl multi read error $exec_return_value\n", E_USER_WARNING);
+		}
+
+		// Extract the content
+		foreach($this->requests as $num => $req) {
+			// Check for errors
+			$curl_error = curl_error($ch[$num]);
+			if($curl_error == "") {
+				$this->requests[$num]['result'] = curl_multi_getcontent($ch[$num]);
+				$this->requests[$num]['info'] = curl_getinfo($ch[$num]);
+			} else {
+				if (!$this->requests[$num]['errors_ok']) {
+					throw new \Exception("Curl error on handle $num: $curl_error");
+				}
+			}
+			
+			// Remove and close the handle
+			curl_multi_remove_handle($mh, $ch[$num]);
+			curl_close($ch[$num]);
+		}
+		
+		// Clean up the curl_multi handle
+		curl_multi_close($mh);
+		
+		// Make the callbacks
+		foreach($this->requests as $num => $req) {
+			$callback = $req['callback'];
+			if ($callback) {
+				$resp = new HttpResponse($req['url'], $req['result'], $req['callback_info'], $req['info']);
+				$callback($resp);
+			}
+		}
+		
+		$this->requests = [];
+	}
+}
+
---
+++ src/HttpResponse.php
@@ -0,0 +1,115 @@
+<?
+
+namespace Cohort;
+
+class HttpResponse {
+	private $url;
+	private $response;
+	private $requestInfo;
+	private $responseInfo;
+	
+	private $responseHeaders = false;
+	private $responseData = false;
+	private $responseHeaderArray = false;
+	private $responseCookies = false;
+	
+	public function __construct($url, $response, $request_info, $response_info) {
+		$this->url = $url;
+		$this->response = $response;
+		$this->requestInfo = $request_info;
+		$this->responseInfo = $response_info;
+	}
+	
+	public function getUrl() { return $this->url; }
+	public function getResponse() { return $this->response; }
+	public function getRequestInfo() { return $this->requestInfo; }
+	public function getResponseInfo() { return $this->responseInfo; }
+	
+	private function parseResponse() {
+		list($this->responseHeaders, $this->responseData) = explode("\r\n\r\n", $this->response, 2);
+	}
+	
+	public function getResponseHeaders() {
+		if ($this->responseHeaders === false) $this->parseResponse();
+		return $this->responseHeaders;
+	}
+	public function getResponseData() {
+		if ($this->responseData === false) $this->parseResponse();
+		return $this->responseData;
+	}
+	
+	private function parseResponseHeaders() {
+		$headers = $this->getResponseHeaders();
+		$headers = str_replace("\r\n", "\n", $headers);
+		$headers = str_replace("\r", "\n", $headers);
+		
+		$this->responseHeaderArray = [];
+		foreach (explode("\n", $headers) as $line) {
+			if (!strpos($line, ":")) {
+				$this->responseHeaderArray['HTTP'] = trim($line);
+				continue;
+			}
+			
+			list($key, $val) = explode(":", $line, 2);
+			$key = strtolower(trim($key));
+			$val = trim($val);
+			if (isset($this->responseHeaderArray[$key])) {
+				if (!is_array($this->responseHeaderArray[$key])) {
+					$tmp = $this->responseHeaderArray[$key];
+					$this->responseHeaderArray[$key] = [ $tmp ];
+				}
+				$this->responseHeaderArray[$key][] = $val;
+			} else {
+				$this->responseHeaderArray[$key] = $val;
+			}
+		}
+	}
+	public function getResponseHeaderArray() {
+		if ($this->responseHeaderArray === false) $this->parseResponseHeaders();
+		return $this->responseHeaderArray;
+	}
+	public function getResponseHeader($key) {
+		$key = strtolower($key);
+		if ($this->responseHeaderArray === false) $this->parseResponseHeaders();
+		if (isset($this->responseHeaderArray[$key])) return $this->responseHeaderArray[$key];
+		return false;
+	}
+	
+	private function parseResponseCookies() {
+		$this->responseCookies = [];
+		$cookie_lines = $this->getResponseHeader("Set-Cookie");
+		if (!$cookie_lines) return;
+		if (!is_array($cookie_lines)) $cookie_lines = [ $cookie_lines ];
+		
+		foreach ($cookie_lines as $cookie_line) {
+			$cookie = [];
+			$cookie_attrs = explode(";", $cookie_line);
+			
+			list($cookie_name, $cookie_value) = explode("=", $cookie_attrs[0], 2);
+			$cookie['value'] = urldecode($cookie_value);
+			
+			for ($i = 1; $i < count($cookie_attrs); $i++) {
+				$attr = trim($cookie_attrs[$i]);
+				if (strpos($attr, "=")) {
+					list($key, $val) = explode("=", $attr, 2);
+				} else {
+					$key = $attr;
+					$val = true;
+				}
+				$key = strtolower($key);
+				
+				$cookie[$key] = $val;
+			}
+			$this->responseCookies[$cookie_name] = $cookie;
+		}
+	}
+	public function getResponseCookies() {
+		if ($this->responseCookies === false) $this->parseResponseCookies();
+		return $this->responseCookies;
+	}
+	public function getResponseCookie($key) {
+		if ($this->responseCookies === false) $this->parseResponseCookies();
+		if (isset($this->responseCookies[$key])) return $this->responseCookies[$key];
+		return false;
+	}
+}
